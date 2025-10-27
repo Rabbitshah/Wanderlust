c35daab1f7ab86580c46ed8d5e6d6b34acfcd2db
Rabbitshah <maanavshah151@gmail.com>
2025-10-27 12:41:27 +0530

    Fixed redirect

--- app.js
+++ app.js
@@ -99,6 +99,7 @@ app.use((req, res, next) => {
 //   res.send(registeredUser);
 // });
 
+app.get("/", (req, res) => res.redirect("/listings"));
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/reviews", reviewRouter);
